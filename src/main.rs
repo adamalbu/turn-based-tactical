c27dcbac3130c0e10c06764cd940e55887a0452e
Adam Albu <45970222+adamalbu@users.noreply.github.com>
2026-03-06 16:04:18 +0000

    window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Tactical Game".into(),
+                name: Some("turn-based-tactical".into()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
