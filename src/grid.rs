6c38ae64b54300c31ff867f90081a5938ab954ff
Adam Albu <45970222+adamalbu@users.noreply.github.com>
2026-03-06 17:16:55 +0000

    tile highlighting

--- src/grid.rs
+++ src/grid.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    picking::hover::{self, Hovered},
+    prelude::*,
+};
 
 pub const TILE_SIZE: f32 = 64.0;
 pub const MAP_WIDTH: u32 = 12;
@@ -12,13 +15,24 @@ pub struct Tile {
     pub y: i32,
 }
 
+#[derive(Component)]
+pub struct TileHighlight;
+
 pub fn spawn(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let hover_mesh = meshes.add(Rectangle::new(TILE_SIZE, TILE_SIZE));
+
     let tile_mesh = meshes.add(Rectangle::new(TILE_SIZE, TILE_SIZE).to_ring(THICKNESS));
     let tile_material = materials.add(Color::BLACK);
+
+    let highlight_mesh = meshes.add(Rectangle::new(
+        TILE_SIZE - THICKNESS * 2.0,
+        TILE_SIZE - THICKNESS * 2.0,
+    ));
+    let highlight_material = materials.add(Color::srgba(1.0, 0.93, 0.34, 0.5));
 
     let offset = Vec2::new(
         -(MAP_WIDTH as f32 * TILE_SIZE) / 2.0 + TILE_SIZE / 2.0,
@@ -29,15 +43,55 @@ pub fn spawn(
         for y in 0..MAP_HEIGHT {
             let pos = Vec2::new(x as f32 * TILE_SIZE, y as f32 * TILE_SIZE) + offset;
 
-            commands.spawn((
-                Mesh2d(tile_mesh.clone()),
-                MeshMaterial2d(tile_material.clone()),
-                Transform::from_xyz(pos.x, pos.y, 0.0),
-                Tile {
-                    x: x as i32,
-                    y: y as i32,
-                },
-            ));
+            commands
+                .spawn((
+                    Mesh2d(hover_mesh.clone()),
+                    Transform::from_xyz(pos.x, pos.y, 1.0),
+                    Tile {
+                        x: x as i32,
+                        y: y as i32,
+                    },
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        Mesh2d(highlight_mesh.clone()),
+                        MeshMaterial2d(highlight_material.clone()),
+                        TileHighlight,
+                        Transform::from_xyz(0.0, 0.0, -0.1),
+                        Visibility::Hidden,
+                    ));
+
+                    parent.spawn((
+                        Mesh2d(tile_mesh.clone()),
+                        MeshMaterial2d(tile_material.clone()),
+                        Transform::from_xyz(0.0, 0.0, -0.2),
+                    ));
+                })
+                .observe(
+                    |mut over: On<Pointer<Over>>,
+                     query: Query<&Children, With<Tile>>,
+                     mut highlights: Query<&mut Visibility, With<TileHighlight>>| {
+                        let children = query.get(over.entity).unwrap();
+                        let child = children.first().unwrap();
+
+                        let mut vis = highlights.get_mut(*child).unwrap();
+                        *vis = Visibility::Visible;
+
+                        over.propagate(false);
+                    },
+                ).observe(
+                    |mut out: On<Pointer<Out>>,
+                     query: Query<&Children, With<Tile>>,
+                     mut highlights: Query<&mut Visibility, With<TileHighlight>>| {
+                        let children = query.get(out.entity).unwrap();
+                        let child = children.first().unwrap();
+
+                        let mut vis = highlights.get_mut(*child).unwrap();
+                        *vis = Visibility::Hidden;
+
+                        out.propagate(false);
+                    },
+                );
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,17 @@ pub fn spawn_camera(mut commands: Commands) {
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::WHITE))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Tactical Game".into(),
-                name: Some("turn-based-tactical".into()),
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Tactical Game".into(),
+                    name: Some("turn-based-tactical".into()),
+                    ..Default::default()
+                }),
                 ..Default::default()
             }),
-            ..Default::default()
-        }))
+            MeshPickingPlugin,
+        ))
         .add_systems(
             Startup,
             (
